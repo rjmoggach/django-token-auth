ee2dcbed5b80e29f29072a175727b58c9cadf473
Robert Moggach <rob@dhcp45.asylumfx.com>
2010-02-16 12:28:35 -0800

    adjusted versioning v0.1a2

--- src/token_auth/__init__.py
+++ src/token_auth/__init__.py
@@ -1,14 +1,17 @@
-VERSION = (0, 1, 'alpha', 2)
+VERSION = (0, 1, 0, 'alpha', 2)
 
 STATUSES = {'alpha': 'a', 'beta': 'b', 'releasecandidiate', 'rc' }
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
     
-    if VERSION[2:] == ('alpha', 0):
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    
+    if VERSION[3:] == ('alpha', 0):
         version = '%s pre-alpha' % version
     
     else:
-        if VERSION[2] != 'final':
-            version = '%s%s%s' % (version, STATUSES[VERSION[2]], VERSION[3])
+        if VERSION[3] != 'final':
+            version = '%s%s%s' % (version, STATUSES[VERSION[3]], VERSION[4])
     return version
